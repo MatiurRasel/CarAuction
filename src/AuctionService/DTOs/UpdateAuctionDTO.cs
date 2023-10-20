0af9d3ed3697374d18dbf2a423690f46ca732f6d
Matiur Rasel <matiur@deshuniversal.com>
2023-10-20 14:39:35 +0600

    changes-add

---
+++ src/AuctionService/DTOs/UpdateAuctionDTO.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionService.DTOs
+{
+    public class UpdateAuctionDTO
+    {
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Color { get; set; }
+        [Required]
+        public int Mileage { get; set; }
+    }
+}
