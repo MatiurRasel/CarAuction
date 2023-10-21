d9e45adef811962957a571753066081017c6e75a
MatiurRasel <matiurrasel1002@gmail.com>
2023-10-21 16:43:59 +0600

    changes-ok

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -40,5 +41,57 @@ namespace AuctionService.Controllers
 
             return _mapper.Map<AuctionDTO>(auction);    
         }
+        [HttpPost]
+        public async Task<ActionResult<AuctionDTO>> CreateAuction(CreateAuctionDTO auctionDTO)
+        {
+            var auction = _mapper.Map<Auction>(auctionDTO);
+            //TODO:  Add current user as seller
+            auction.Seller = "test";
+            _context.Auctions.Add(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+            if(!result) return BadRequest("Could not save changes to the database");
+
+            return CreatedAtAction(nameof(GetAuctionById), new {auction.Id},
+            _mapper.Map<AuctionDTO>(auction));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id,UpdateAuctionDTO updateAuctionDTO)
+        {
+            var auction =  await _context.Auctions
+                .Include(x =>x.Item)
+                .FirstOrDefaultAsync(x =>x.Id == id);
+
+            if(auction == null) return NotFound();
+
+            //TODO: check seller == userName
+
+            auction.Item.Make = updateAuctionDTO.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDTO.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDTO.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDTO.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDTO.Year ?? auction.Item.Year;
+
+            var result = await _context.SaveChangesAsync() > 0;
+            if(result) return Ok();
+            return BadRequest("Problem saving changes");
+        } 
+    
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+            if(auction == null) return NotFound();
+
+            //TODO: check seller == userName
+
+            _context.Auctions.Remove(auction);
+
+            var result = await _context.SaveChangesAsync() >0;
+            if(!result) return BadRequest("Could not update  in database");
+            return Ok();
+        }
+    
     }
 }
--- src/AuctionService/DTOs/UpdateAuctionDTO.cs
+++ src/AuctionService/DTOs/UpdateAuctionDTO.cs
@@ -1,22 +1,18 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AuctionService.DTOs
 {
     public class UpdateAuctionDTO
     {
-        [Required]
+        
         public string Make { get; set; }
-        [Required]
+        
         public string Model { get; set; }
-        [Required]
-        public int Year { get; set; }
-        [Required]
+       
+        public int? Year { get; set; }
+   
         public string Color { get; set; }
-        [Required]
-        public int Mileage { get; set; }
+       
+        public int? Mileage { get; set; }
     }
 }
