f170609bf0d6f79928dc5205f570159de90f7e95
MatiurRasel <matiurrasel1002@gmail.com>
2023-10-23 15:58:06 +0600

    add-changes

--- src/SearchService/Services/AuctionSvcHttpClient.cs
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -1,3 +1,6 @@
+using MongoDB.Entities;
+using SearchService.Models;
+
 namespace SearchService.Services
 {
     public class AuctionSvcHttpClient
@@ -11,6 +14,18 @@ namespace SearchService.Services
             _config = config;
         }
 
-        public async Task
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            var lastUpdated = await DB.Find<Item, string>()
+                .Sort(x=>x.Descending(x=>x.UpdatedAt))
+                .Project(x=>x.UpdatedAt.ToString())
+                .ExecuteFirstAsync();
+
+            return await _httpClient
+                .GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"]
+                + "/api/auctions?date=" + lastUpdated);
+
+                
+        }
     }
 }
