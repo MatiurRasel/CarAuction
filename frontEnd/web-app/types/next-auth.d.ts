0e7b593bcbc1381ee3d899fdffd572b9c5f3eda4
MatiurRasel <matiurrasel1002@gmail.com>
2023-12-04 17:31:21 +0600

    Added-Changes

--- frontEnd/web-app/types/next-auth.d.ts
+++ frontEnd/web-app/types/next-auth.d.ts
@@ -3,12 +3,17 @@ import { DefaultSession } from "next-auth";
 declare module 'next-auth' {
     interface Session {
         user: {
+            id: string
             userName: string
         } & DefaultSession['user']
     }
 
     interface Profile {
         userName:string
+    }
+
+    interface User {
+        userName: string
     }
 }
 
