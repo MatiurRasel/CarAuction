6bfaee6a314082fded8623d4b52cd4bcf17d609c
MatiurRasel <matiurrasel1002@gmail.com>
2023-12-05 19:05:24 +0600

    Added-Changes

--- frontEnd/web-app/app/actions/auctionAction.ts
+++ frontEnd/web-app/app/actions/auctionAction.ts
@@ -3,28 +3,36 @@
 import { Auction, PagedResult } from "@/types";
 import { getTokenWorkAround } from "./authActions";
 import { fetchWrapper } from "@/lib/fetchWrapper";
+import { FieldValue, FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query: string) : Promise<PagedResult<Auction>>{
     return await fetchWrapper.get(`search${query}`)
 }
 
-export async function UpdateAuctionTest() {
+export async function updateAuctionTest() {
     const data = {
         mileage: Math.floor(Math.random() * 100000) + 1
     }
 
     const token = await getTokenWorkAround();
+    
+    return await fetchWrapper.put('auctions/642fa911-b04b-43bb-be7e-31707b970a4b',
+    data);
 
-    const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c',{
-        method:'PUT',
-        headers:{
-            'Content-type': 'application/json',
-            'Authorization': 'Bearer '+token?.access_token
-        },
-        body:JSON.stringify(data)
-    })
+}
 
-    if(!res.ok) return {status: res.status,message: res.statusText}
+export async function createAuction(data: FieldValues) {
+    return await fetchWrapper.post('auctions',data);
+}
 
-    return res.statusText;
+export async function getDetailedViewData(id: string) : Promise<Auction> {
+    return await fetchWrapper.get(`auctions/${id}`);
+}
+
+export async function updateAuction(data: FieldValues,id: string) {
+    const res = await fetchWrapper.put(`auctions/${id}`,data);
+    revalidatePath(`/auctions/${id}`);
+    return res;
+
 }
