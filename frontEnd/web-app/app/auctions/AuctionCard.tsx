1f3532553888f4ebbd3e07e1ffaf61dec8309ebf
MatiurRasel <matiurrasel1002@gmail.com>
2023-12-13 16:32:27 +0600

    added-chages

--- frontEnd/web-app/app/auctions/AuctionCard.tsx
+++ frontEnd/web-app/app/auctions/AuctionCard.tsx
@@ -3,6 +3,7 @@ import CountdownTimer from './CountdownTimer'
 import CarImage from './CarImage'
 import { Auction } from '@/types'
 import Link from 'next/link'
+import CurrentBid from './CurrentBid'
 
 type Props = {
     auction: Auction
@@ -16,7 +17,13 @@ export default function AuctionCard({auction}: Props) {
            <div>
            <CarImage imageUrl={auction.imageUrl}/>
             <div className='absolute bottom-2 left-2'>
-            <CountdownTimer auctionEnd={auction.auctionEnd} />
+              <CountdownTimer auctionEnd={auction.auctionEnd} />
+            </div>
+
+            <div className='absolute top-2 right-2'>
+              <CurrentBid 
+                reservePrice={auction.reservePrice} 
+                amount={auction.currentHighBid} />
             </div>
            </div>
            
---
+++ frontEnd/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+type Props = {
+    
+}
+
+import React from 'react'
+
+export default function BidForm() {
+  return (
+    <div></div>
+  )
+}
